a65e7a9850b6385bb6e584b722dc18fa0511abae
Daniel Konrad <mail@daniel-konrad.com>
2022-04-18 19:56:41 +0200

    getting liveticker page

--- liveticker2csv.py
+++ liveticker2csv.py
@@ -1,2 +1,10 @@
 # file: liveticker2csv.py
+import httpx
 
+def get_livetickerpage(url: str):
+    page = httpx.get(url)
+    return page.text
+
+
+if __name__ == "__main__":
+    print(get_livetickerpage("https://livecenter.sportschau.de/fussball/deutschland-bundesliga/ma9242973/vfb-stuttgart_borussia-dortmund/liveticker/"))
